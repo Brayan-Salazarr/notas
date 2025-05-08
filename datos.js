7b528203481b5a911af896ed33c2685f9f3b3474
LauraAG98 <lauraguirregarcia@outlook.com>
2025-05-07 23:02:33 -0500

    AGREGADO funcion guardarNombre y se agrega cuadro de informacion en el index

---
+++ datos.js
@@ -0,0 +1,4 @@
+function guardarNombre() {
+    let nombreIngre=document.getElementById("nombreIngre").value;
+    document.getElementById("resultado").textContent = "Nombre de estudiante: " + nombreIngre;
+}
