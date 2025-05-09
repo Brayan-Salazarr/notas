fd4dea1ef5f6b2152d75aff8a4e573ffddf6ead1
LauraAG98 <lauraguirregarcia@outlook.com>
2025-05-08 23:04:58 -0500

    Se corrige error en los valores de las notas

--- datos.js
+++ datos.js
@@ -11,9 +11,13 @@ function guardarMateria() {
 let nota1, nota2, nota3;
 
 function promedio(){
-    nota1=prompt("Ingrese primer nota")
-    nota2=prompt("Ingrese segunda nota")
-    nota3=prompt("Ingrese tercer nota")
+    nota1=prompt("Ingrese primer nota");
+    nota2=prompt("Ingrese segunda nota");
+    nota3=prompt("Ingrese tercer nota");
+
+    nota1=Number(nota1);
+    nota2=Number(nota2);
+    nota3=Number(nota3);
 
     let prome=(nota1+nota2+nota3)/3;
 
