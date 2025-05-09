f26c94000ac0cae637424e3063508fb43a5356e6
Brayan-Salazarr <brayansalazzar1221@gmail.com>
2025-05-08 22:41:28 -0500

    AGRGO ingreso de notas de la funcion de promedio

--- datos.js
+++ datos.js
@@ -6,6 +6,9 @@ function guardarNombre() {
 let nota1, nota2, nota3;
 
 function promedio(){
+    nota1=prompt("Ingrese primer nota")
+    nota2=prompt("Ingrese segunda nota")
+    nota3=prompt("Ingrese tercer nota")
 
     let prome=(nota1+nota2+nota3)/3;
     document.write("Su nota final es: "+prome.toFixed(2));
